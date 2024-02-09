cce8f714c44c7ad3cf51394119b2a3a2e01c2b5c
2CHEVSKII <prsroman3@gmail.com>
2024-02-10 01:44:10 +0300

    feat: extensions for Nullable<> types

---
+++ src/Dvchevskii.Optional/Extensions/NullableExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Dvchevskii.Optional.Extensions
+{
+    public static class NullableExtensions
+    {
+        public static Option<T> NoneIfNull<T>(this T? nullable) where T : struct
+        {
+            if (nullable.HasValue)
+            {
+                return Option.Some(nullable.Value);
+            }
+
+            return Option.None<T>();
+        }
+    }
+}
---
+++ test/Dvchevskii.Optional.Tests/NullableExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using Dvchevskii.Optional.Extensions;
+using FluentAssertions;
+
+namespace Dvchevskii.Optional.Tests;
+
+[TestClass]
+public class NullableExtensionsTests
+{
+    [TestMethod]
+    public void Test_NoneIfNull()
+    {
+        AsNullable<int>(42).NoneIfNull().IsSomeAnd(x => x == 42).Should().BeTrue();
+        AsNullable<double>(null).NoneIfNull().IsNone().Should().BeTrue();
+        AsNullable<float>(69.0f).NoneIfNull().IsSomeAnd(x => x == 69.0f).Should().BeTrue();
+        AsNullable<bool>(null).NoneIfNull().IsNone().Should().BeTrue();
+    }
+
+    private static T? AsNullable<T>(T? val)
+        where T : struct
+    {
+        return val;
+    }
+}
