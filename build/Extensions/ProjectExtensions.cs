48b21771aecf0d5385da5a03907f1863dbcbcdc8
2CHEVSKII <prsroman3@gmail.com>
2024-03-11 02:52:46 +0300

    chore: add coverage tasks to build

--- build/Extensions/ProjectExtensions.cs
+++ build/Extensions/ProjectExtensions.cs
@@ -1,6 +1,27 @@
-﻿using Nuke.Common.ProjectModel;
+﻿using System.Linq;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
 
 static class ProjectExtensions
 {
     public static bool IsPackable(this Project project) => project.GetProperty<bool>("IsPackable");
+
+    public static AbsolutePath GetBuildDirectory(
+        this Project project,
+        Configuration configuration,
+        string targetFramework = null
+    ) =>
+        project.Directory
+        / "bin"
+        / configuration
+        / (targetFramework ?? project.GetDefaultTargetFramework());
+
+    public static AbsolutePath GetAssemblyOutputPath(
+        this Project project,
+        Configuration configuration,
+        string targetFramework = null
+    ) => project.GetBuildDirectory(configuration, targetFramework) / $"{project.Name}.dll";
+
+    public static string GetDefaultTargetFramework(this Project project) =>
+        project.GetTargetFrameworks()!.First();
 }
