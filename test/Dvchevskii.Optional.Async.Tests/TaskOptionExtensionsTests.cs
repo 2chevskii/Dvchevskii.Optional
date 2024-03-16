6698eecd14c862db78bd49c46a19287b453c31a4
2CHEVSKII <prsroman3@gmail.com>
2024-03-16 04:57:23 +0300

    test: WIP async tests

---
+++ test/Dvchevskii.Optional.Async.Tests/TaskOptionExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace Dvchevskii.Optional.Async.Tests;
+
+[TestClass]
+public class TaskOptionExtensionsTests
+{
+    [TestMethod]
+    public async Task Test_IsSomeAsync()
+    {
+        var t1 = new Task<Option<int>>(() => Option.Some(42));
+        t1.Start();
+        (await t1.IsSomeAsync()).Should().BeTrue();
+        (await Task.FromResult(Option.Some(42)).IsSomeAsync()).Should().BeTrue();
+        (await Task.Run(() => Option.Some(42)).IsSomeAsync()).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public async Task Test_AsyncBasic()
+    {
+        (await Task.FromResult(Option.Some(42))).Unwrap().Should().Be(42);
+        (await Task.FromResult(Option.Some(69))).Unwrap().Should().Be(69);
+        (await Task.Run(() => Option.Some(42))).Unwrap().Should().Be(42);
+        (await Task.Run(() => Option.Some(69))).Unwrap().Should().Be(69);
+
+        var t1 = new Task<Option<int>>(() => Option.Some(42));
+        t1.Start();
+        var t2 = new Task<Option<int>>(() => Option.Some(69));
+        t2.Start();
+
+        (await t1).Unwrap().Should().Be(42);
+        (await t2).Unwrap().Should().Be(69);
+    }
+}
