6afb420bac0f0f5fcdec7b4b7fc2d59429f8264e
2CHEVSKII <prsroman3@gmail.com>
2024-03-17 23:52:51 +0300

    feat: WIP function wrappers

---
+++ src/Dvchevskii.Optional/Extensions/FunctionWrappersExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Dvchevskii.Optional.Extensions
+{
+    public static class FunctionWrappersExtensions
+    {
+        public static unsafe Option<Unit.Unit> CallSafe(delegate*<void> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Option<Unit.Unit> CallSafe(this Action action)
+        {
+            try
+            {
+                action();
+                return Option.Some(Unit.Unit.Default);
+            }
+            catch (Exception _)
+            {
+                return Option.None<Unit.Unit>();
+            }
+        }
+
+        public static Func<Option<Unit.Unit>> MakeSafe(this Action action)
+        {
+            return () =>
+            {
+                try
+                {
+                    action();
+                    return Option.Some(Unit.Unit.Default);
+                }
+                catch (Exception _)
+                {
+                    return Option.None<Unit.Unit>();
+                }
+            };
+        }
+
+        public static Func<Option<TResult>> MakeSafeNoExcept<TResult>(this Func<TResult> func)
+        {
+            return () =>
+            {
+                try
+                {
+
+                    return Option.Some(func());
+                }
+                catch (Exception _)
+                {
+                    return Option.None<TResult>();
+                }
+            };
+        }
+
+        public static Func<Option<TResult>, Exception> MakeSafe<TResult>(this Func<TResult> func)
+        {
+
+        }
+    }
+}
