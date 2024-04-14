11599ebdf70f9aa6cefdad5319b3ccf52e48e76d
2CHEVSKII <prsroman3@gmail.com>
2024-04-14 05:46:40 +0300

    chore: update PatternMatchingTests

--- test/Dvchevskii.Optional.Tests/PatternMatchingTests.cs
+++ test/Dvchevskii.Optional.Tests/PatternMatchingTests.cs
@@ -1,36 +1,27 @@
-﻿#pragma warning disable CS8509 // The switch expression does not handle all possible values of its input type (it is not exhaustive).
+﻿using FluentAssertions;
+
+#pragma warning disable CS8509 // The switch expression does not handle all possible values of its input type (it is not exhaustive).
 
 namespace Dvchevskii.Optional.Tests;
 
 [TestClass]
 public class PatternMatchingTests
 {
-    private Option<int> _some = null!;
-    private Option<int> _none = null!;
-
-    [TestInitialize]
-    public void Initialize()
+    [TestMethod]
+    public void Test_SwitchExpression()
     {
-        _some = Option.Some(42);
-        _none = Option.None<int>();
+        bool someMatched = Option.Some(42) switch
+        {
+            Some<int> => true,
+            None<int> => false
+        };
+        bool noneMatched = Option.None<int>() switch
+        {
+            Some<int> => true,
+            None<int> => false
+        };
+
+        someMatched.Should().BeTrue();
+        noneMatched.Should().BeFalse();
     }
-
-    /*[TestMethod]
-    public void Test_Match()
-    {
-        (
-            _some switch
-            {
-                ISome => 0,
-                INone => 1
-            }
-        ).Should().Be(0);
-        (
-            _none switch
-            {
-                ISome => 0,
-                INone => 1
-            }
-        ).Should().Be(1);
-    }*/
 }
