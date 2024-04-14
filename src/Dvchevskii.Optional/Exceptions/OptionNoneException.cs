cd050aa5a37fea16bab4d01bf6b6b293748a6b36
2CHEVSKII <prsroman3@gmail.com>
2024-04-14 05:31:39 +0300

    chore: rename exception

---
+++ src/Dvchevskii.Optional/Exceptions/OptionNoneException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dvchevskii.Optional.Exceptions
+{
+    public sealed class OptionIsNoneException : Exception
+    {
+        public OptionIsNoneException()
+        {
+
+        }
+
+        public OptionIsNoneException(string message) : base(message)
+        {
+
+        }
+    }
+}
