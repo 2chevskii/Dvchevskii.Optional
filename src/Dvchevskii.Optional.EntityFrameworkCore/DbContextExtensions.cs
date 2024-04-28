5514fd31d87ce75db65cdf2ba2343c09699fe342
2CHEVSKII <prsroman3@gmail.com>
2024-04-28 15:43:38 +0300

    feat: extend range of supported efcore versions and runtimes by Dvchevskii.Optional.EntityFrameworkCore

--- src/Dvchevskii.Optional.EntityFrameworkCore/DbContextExtensions.cs
+++ src/Dvchevskii.Optional.EntityFrameworkCore/DbContextExtensions.cs
@@ -4,28 +4,29 @@ using Dvchevskii.Optional.Async;
 using Dvchevskii.Optional.Extensions;
 using Microsoft.EntityFrameworkCore;
 
-namespace Dvchevskii.Optional.EntityFrameworkCore;
-
-public static class DbContextExtensions
+namespace Dvchevskii.Optional.EntityFrameworkCore
 {
-    public static Option<object> FindOrNone(
-        this DbContext dbContext,
-        Type entityType,
-        params object[] keyValues
-    ) => dbContext.Find(entityType, keyValues).AsOption();
+    public static class DbContextExtensions
+    {
+        public static Option<object> FindOrNone(
+            this DbContext dbContext,
+            Type entityType,
+            params object[] keyValues
+        ) => dbContext.Find(entityType, keyValues).AsOption();
 
-    public static Option<T> FindOrNone<T>(this DbContext dbContext, params object[] keyValues)
-        where T : class => dbContext.Find<T>(keyValues).AsOption();
+        public static Option<T> FindOrNone<T>(this DbContext dbContext, params object[] keyValues)
+            where T : class => dbContext.Find<T>(keyValues).AsOption();
 
-    public static Task<Option<object>> FindOrNoneAsync(
-        this DbContext dbContext,
-        Type entityType,
-        params object[] keyValues
-    ) => dbContext.FindAsync(entityType, keyValues).AsTask().AsOptionAsync();
+        public static Task<Option<object>> FindOrNoneAsync(
+            this DbContext dbContext,
+            Type entityType,
+            params object[] keyValues
+        ) => dbContext.FindAsync(entityType, keyValues).AsTask().AsOptionAsync();
 
-    public static Task<Option<T>> FindOrNoneAsync<T>(
-        this DbContext dbContext,
-        params object[] keyValues
-    )
-        where T : class => dbContext.FindAsync<T>(keyValues).AsTask().AsOptionAsync();
+        public static Task<Option<T>> FindOrNoneAsync<T>(
+            this DbContext dbContext,
+            params object[] keyValues
+        )
+            where T : class => dbContext.FindAsync<T>(keyValues).AsTask().AsOptionAsync();
+    }
 }
--- src/Dvchevskii.Optional.EntityFrameworkCore/DbSetExtensions.cs
+++ src/Dvchevskii.Optional.EntityFrameworkCore/DbSetExtensions.cs
@@ -3,13 +3,14 @@ using Dvchevskii.Optional.Async;
 using Dvchevskii.Optional.Extensions;
 using Microsoft.EntityFrameworkCore;
 
-namespace Dvchevskii.Optional.EntityFrameworkCore;
-
-public static class DbSetExtensions
+namespace Dvchevskii.Optional.EntityFrameworkCore
 {
-    public static Option<T> FindOrNone<T>(this DbSet<T> set, params object[] keyValues)
-        where T : class => set.Find(keyValues).AsOption();
+    public static class DbSetExtensions
+    {
+        public static Option<T> FindOrNone<T>(this DbSet<T> set, params object[] keyValues)
+            where T : class => set.Find(keyValues).AsOption();
 
-    public static Task<Option<T>> FindOrNoneAsync<T>(this DbSet<T> set, params object[] keyValues)
-        where T : class => set.FindAsync(keyValues).AsTask().AsOptionAsync();
+        public static Task<Option<T>> FindOrNoneAsync<T>(this DbSet<T> set, params object[] keyValues)
+            where T : class => set.FindAsync(keyValues).AsTask().AsOptionAsync();
+    }
 }
