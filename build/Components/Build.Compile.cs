075010c92db8686a6c659e9c0db9308e58bdefef
2CHEVSKII <prsroman3@gmail.com>
2024-04-28 20:21:15 +0300

    feat(build): add basic project dependency graph resolution to fix compilation errors in Dvchevskii.Optional.MethodWrappers

--- build/Components/Build.Compile.cs
+++ build/Components/Build.Compile.cs
@@ -1,7 +1,12 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
 using Serilog;
 
 [SuppressMessage("ReSharper", "AllUnderscoreLocalParameterName")]
@@ -16,7 +21,49 @@ partial class Build
                     () => Log.Information("Building SRC projects: {Projects}", SrcProjects.Names()),
                     () => Log.Information("Version: {Version}", Version.SemVer),
                     () =>
-                        DotNetTasks.DotNetBuild(
+                    {
+                        List<string> builtProjects = new();
+
+                        void BuildProject(Project project, IEnumerable<Project> allProjects)
+                        {
+                            var references = project
+                                .GetItems("ProjectReference")
+                                .Select(reference =>
+                                {
+                                    AbsolutePath referencePath = AbsolutePath.Create(
+                                        project.Directory + (RelativePath)reference
+                                    );
+
+                                    var projectName = referencePath.NameWithoutExtension;
+                                    return projectName;
+                                });
+
+                            Log.Information(
+                                "Building project {ProjectName}, references: {@References}",
+                                project.Name,
+                                references
+                            );
+
+                            foreach (string reference in references)
+                            {
+                                var referenceProject = allProjects.First(x => x.Name == reference);
+
+                                BuildProject(referenceProject, allProjects);
+                            }
+
+                            if (!builtProjects.Contains(project.Name))
+                            {
+                                DotNetTasks.DotNetBuild(
+                                    settings =>
+                                        settings.Apply(BuildSettingsBase).SetProjectFile(project)
+                                );
+                                builtProjects.Add(project.Name);
+                            }
+                        }
+
+                        SrcProjects.ForEach(x => BuildProject(x, SrcProjects));
+
+                        /*DotNetTasks.DotNetBuild(
                             settings =>
                                 settings
                                     .Apply(BuildSettingsBase)
@@ -24,7 +71,8 @@ partial class Build
                                         SrcProjects,
                                         (settings, project) => settings.SetProjectFile(project)
                                     )
-                        )
+                        );*/
+                    }
                 );
 
     Target CompileTests =>
